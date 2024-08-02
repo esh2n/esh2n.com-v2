d2c577e10b40d085d6ee6ad82fce35407ea5efe5
esh2n <shunyaEndoh.1215@gmail.com>
2024-08-02 15:02:24 +0900

    🔧 Fix: dir

--- frontend/packages/ui/.storybook/preview.ts
+++ frontend/packages/ui/.storybook/preview.ts
@@ -2,21 +2,21 @@ import type { Preview } from "@storybook/web-components";
 import { html } from "lit";
 
 const preview: Preview = {
-  decorators: [
-    (story) => {
-      return html`
+	decorators: [
+		(story) => {
+			return html`
       <div style="font-family: 'Fira Code';">${story()}</div>
       `;
-    },    
-  ],
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+		},
+	],
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
 };
 
 export default preview;
