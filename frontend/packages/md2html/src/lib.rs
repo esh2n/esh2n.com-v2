edb0a62c493a1005079b1342deff5a4d77db7346
esh2n <shunyaEndoh.1215@gmail.com>
2024-08-23 19:00:00 +0900

    🔧 Fix: table support

--- frontend/packages/md2html/src/lib.rs
+++ frontend/packages/md2html/src/lib.rs
@@ -4,7 +4,11 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 pub fn render_markdown(markdown: &str) -> String {
     let mut options = Options::empty();
+    options.insert(Options::ENABLE_TABLES);
+    options.insert(Options::ENABLE_FOOTNOTES);
     options.insert(Options::ENABLE_STRIKETHROUGH);
+    options.insert(Options::ENABLE_TASKLISTS);
+    options.insert(Options::ENABLE_SMART_PUNCTUATION);
     let parser = Parser::new_ext(markdown, options);
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
